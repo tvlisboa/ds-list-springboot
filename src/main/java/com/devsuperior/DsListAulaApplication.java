2600e8baffd616f128a4f8c9261017b349904b52
tvlisboa <tvlisboa@hotmail.com>
2025-08-18 13:57:18 -0300

    Project created

---
+++ src/main/java/com/devsuperior/DsListAulaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListAulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsListAulaApplication.class, args);
+    }
+
+}
